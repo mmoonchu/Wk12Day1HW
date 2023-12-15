98da70c7fd097949f854e710bb3043fe31313b33
Moon Chu <mmoonchu2@gmail.com>
2023-12-14 23:11:11 -0500

    Index.jsx view rendered at pokemon route

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
 const pokemon = require('./models/pokemon');
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
 let port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App!');
 })
 app.get('/pokemon', (req, res) => {
-    res.send(pokemon);
+    res.render('Index', {
+        pokemons: pokemon
+    });
 })
 
 // run
-app.listen(port, () => {
+app.listen(port, (req, res) => {
     console.log(`server be running`);
 })
---
+++ views/Index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const myStyle = {
+    color: '#ffffff',
+    backgroundColor: '#000000',
+}
+
+function Index(props) {
+
+  return (
+    <div style={myStyle}>
+        <h1>See All The Pokemon!</h1>
+    </div>
+  )
+}
+
+export default Index
